1c1a9b5e7d3f5d5a65a05ce3df7f680f2a3f1636
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-03-04 09:55:54 +0400

    get all students from db

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
-//        connect();
-
-        System.out.println(getAllTeacher());
-
-    }
-
+    //    Connection to Student database
     public static Connection connect() throws Exception {
+
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/task2";
         String userName = "postgres";
         String password = "12131";
-        Connection connection = DriverManager.getConnection(url, userName, password);
 
-        if (connection != null) {
-            System.out.println("Connected to DB");
+        Connection con = DriverManager.getConnection(url, userName, password);
+
+        if (con != null) {
+            System.out.println("Connected to Database!");
         }
 
-        return connection;
+        return con;
     }
 
-    public static List<Teacher> getAllTeacher() throws Exception {
-        List<Teacher> res = new ArrayList<>();
+//Getting Student info from database
+
+    public static List<Student> getAllStudents() throws Exception {
+
+        List<Student> res = new ArrayList<>();
+
         try (Connection con = connect();) {
             Statement st = con.createStatement();
-            st.execute("SELECT * FROM teacher");
+            st.execute("SELECT * FROM student");
             ResultSet rs = st.getResultSet();
 
             while (rs.next()) {
 
-                int id = rs.getInt("id");
+                long id = rs.getLong("id");
                 String name = rs.getString("name");
                 String surname = rs.getString("surname");
-                res.add(new Teacher(id, name, surname));
+                String major = rs.getString("major");
+                double fee = rs.getDouble("fee");
+
+                res.add(new Student(id, name, surname, major, fee));
             }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
-        }
 
+        }
         return res;
+    }
+
+
+    //    Main Method
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(getAllStudents());
 
     }
 }
